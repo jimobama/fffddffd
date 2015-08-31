aee7c84d89ac122e19ff6b203cd0cd386c06138f
hacks <hacks@hacks-Lenovo-Yoga-2-13>
2015-08-31 01:25:44 +0100

    Re-structuring code to fit database table

--- Controllers/TryCreateUserController.php
+++ Controllers/TryCreateUserController.php
@@ -13,9 +13,9 @@
  * @author hacks
  */
 include_once("Controllers/IController.php");
-include_once("Models/TryCreateUser.php");
-include_once("Models/TryFetchUser.php");
-include_once("Models/TrySendEmail.php");
+include_once("Models/user_classes/TryCreateUser.php");
+include_once("Models/user_classes/TryFetchUser.php");
+include_once("Includes/TrySendEmail.php");
 
 class TryCreateUserController implements IController{
     
@@ -23,78 +23,72 @@ class TryCreateUserController implements IController{
     public function Index() {        
         
         $email= HttpResquestHandler::RequestParams("email");
-        $fullname= HttpResquestHandler::RequestParams("fullname");
-        $password= HttpResquestHandler::RequestParams("password"); 
-        return $this->Create($email,$fullname,$password);
-        
+        $fullname= HttpResquestHandler::RequestParams("fullname");       
+        $gender= HttpResquestHandler::RequestParams("gender"); 
+        return $this->Create($email,$fullname, $gender);
     }
 
     
     //Create new user accunt
     
-    public function Create($email,$fullname,$password,$long=0,$lat=0)
+    public function Create($email,$fullname, $gender)
     {
         
         $jsonView = new JsonViewer();
         $response= array();
-        
+        $response["success"]=0;
+        $user = new User();
+        $user->setEmail($email)
+                ->setFullname($fullname)
+                ->setId(Validator::UniqueKey())
+                ->setGender($gender);
+              
+       
       
-       
-       $tryFetchUser= new  TryFetchUser(new UserTable());
-   
-       $valid=  $this->validate($email,$fullname,$password);
-       
-       //try to fetch the user with the email address if it return null that means the email does not exist
-       if($tryFetchUser->fetch($email)==null &&  $valid)
-       {
-            $user= new  TryCreateUser($email,$fullname,$password,$long,$lat);
-     
-       
-           $abool= $user->create();
-           if($abool){
-               
-                $response["success"]=1;                
-                $user= $tryFetchUser->fetch($email);
-                $response["user"]=$user;
-                
-                $this->sendWelcomeMessage($password, json_encode($user));
-           }else{
-                $response["success"]=0;
-                $response["error_message"]="report this problem please!";
-           }
+       $valid=  $this->validate($user);
+       if($valid){
+           
+          $response=$this->tryCreate($user);
            
        }else{
-           //if its returns a valid that means the email address already exists
-           $response["success"]=0;
-           if($valid){
-                $response["error_message"]="The  [$email] account already exists ";
-           }else{
-               $response["error_message"]=$this->message;
-           }
+          $response["error_message"] =$this->message;
        }
-      
-       $jsonView->setContent($response);
        
+       //try to fetch the user with the email address if it return null that means the email does not exist            
+       $jsonView->setContent($response);       
        return  $jsonView ;
         
     }
 //put your code here
     
     
-    private function validate($email,$fullname,$password)
+    private function validate(User $user)
     {
         
         $okay=false;
+        if( $user==null){ return $okay;}
         
-         if(!Validator::isEmail($email))
+         if(!Validator::isEmail( $user->getEmail()))
          {
+             $email =$user->getEmail();
+             if($email=="" || $email==null){
+              $this->message="User email address required";
+             }else{
              $this->message="Invalid email[$email] entry";
-         }elseif(!Validator::IsWord($fullname))
+             }
+             
+         }elseif(!Validator::IsWord($user->getFullname()))
          {
-               $this->message="Invalid name entry [$fullname] ";
-         }else if(!Validator::IsWord($password))
+             $fullname=$user->getFullname();
+             if($fullname==null || $fullname==""){
+                $this->message="User fullname is required!";   
+             }else{
+             
+                 $this->message="Enter a valid user name ";
+             }
+         }else if(Validator::IsWord($user->getGender()))
          {
-             $this->message="Password should be a combination of letters and words";
+             $this->message="Enter your gender";
          }else{
              $okay=true;
          }
@@ -103,15 +97,49 @@ class TryCreateUserController implements IController{
         
     }
     
+    //the helper method will create the user
+    private function tryCreate(User $user)
+    {
+     $tryFetchUser= new  TryFetchUser();
+     $response=array();
+     if($tryFetchUser->fetch($user->getEmail())==null)
+       {
+            $tryCreate= new  TryCreateUser($user);       
+            $abool=  $tryCreate->create();
+           
+           if($abool){
+               
+                $response["success"]=1;                
+                $user= $tryFetchUser->fetch($user->getEmail());
+                $response["user"]=$user;
+                
+                $this->sendWelcomeMessage($user->getPassword(), json_encode($user));
+           }else{
+                $response["success"]=0;
+                $response["error_message"]="report this problem please!";
+           }
+           
+       }else{
+           $email=$user->getEmail();
+           $response["error_message"]="The [$email] account already exists ";
+       }
+       return $response;
+    }
+    
+    
+    
     //Do care if it send or not
     private function sendWelcomeMessage($raw_password, $json)
     {
         
-        $user = json_decode($json);
+        $user = new User($json);
         
         if($user !=null)
         {
-            $message = "Dear $user->Fullname, \n\t Thanks for register with DEvent , we intend to bring you new events around the world to you.\n"
+            $fullname=$user->getFullname();
+            $code=$user->getVerificationCode();
+           
+            $message = "Dear  $fullname, \n\t Thanks for register with DFinder , we intend to bring you new events around the world to you.\n"
                     . "The DEvents is a social application that search events around in your location  with just a clicked.\n"
                     . "\n <br> "
                     . "Below is your username and password"
@@ -119,12 +147,13 @@ class TryCreateUserController implements IController{
                     . "Username = $user->Email\n"
                     . "Password = $raw_password"
                     . "\n"
-                    . "To learn more on how to used DEvent please visit http://www.DEvents.com\n";
+                    . "To verify your account you need to copy this given code below and paste it on the field at your screen\n<br>"
+                    . "Verication code : $code";
             
-            $from="DEventsAdmins@NoReply.com";
+            $from="dfinderadministrator@noreply.com";
             $to=$user->Email;
             $name=$user->Fullname;
-            $subject="DEvent account created comfimation";
+            $subject="DFinder account creation confirmation details";
             
             
             $trysent = new   TrySendEmail($message, $from, $to ,$name,$subject);
